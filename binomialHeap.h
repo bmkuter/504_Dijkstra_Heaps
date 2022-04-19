021c5c990832114aa63d0fbc823b878616c0bafd
Ian Chadwick <ianjchadwick@gmail.com>
2022-04-19 17:36:16 -0400

    Initial commit for Binomial Heap
    Still WIP but here's what is done so far.

---
+++ binomialHeap.h
@@ -0,0 +1,247 @@
+#ifndef BINOMIAL_HEAP_H_
+#define BINOMIAL_HEAP_H_
+
+#include <iostream>
+#include <list>
+#include<algorithm>
+
+using namespace std;
+
+ template <class Object>
+ struct bin_node{
+        bin_node *parent;              // Pointer to parent node
+        bin_node *child;               // Pointer to child node
+        bin_node *lsibling, *rsibling;  // Pointer to left and right sibling nodes
+        int key;                       // Key value for priority
+        int degree;                    // Degree of the node
+        Object * item;                 // Storing the pointer to the <Object>
+ };
+
+
+template <class Object>
+class BinomialHeap
+{
+    private:
+    
+   
+
+
+    public:
+        /* MAKE PRIVATE AFTER TESTING */
+        bin_node<Object> * head;        // Points to the root of the leftmost binomial tree
+        bin_node<Object> * minRoot;     // Points to the root of the tree with smallest key
+        bin_node<Object> * array[40];   // To keep track of degree for merging
+        int size;                       // Number of elements in the Heap
+
+        // Heap constructor initializes all pointers to nullptr and size to 0
+        BinomialHeap()
+        {
+            head = nullptr;
+            minRoot = nullptr;
+            size = 0;
+            for (int i = 0; i < 40; i++)
+            {
+                array[i] = nullptr;
+            }
+        };
+
+/*************************************************************************************
+                            Insepction
+*************************************************************************************/
+        
+        /* Returns true if the heap is empty, false otherwise 
+        Worst case Complexity: O(1)
+        */
+        bool isEmpty(){
+            return this->size == 0;
+        }
+
+/*************************************************************************************
+                            Insertion/Deletion
+*************************************************************************************/
+        
+        /* Lazy insert - inserts new tree nodes at the root level to the left of head in order to decrease time to O(1)
+            input: object* and key
+            output: void -> adds to this heap
+            Worst case complexity: O(1)*/
+        void insert(Object* item, int key){
+            bin_node<Object>* node = new bin_node<Object>;
+            node->key = key;
+            node->item = item;
+            node->degree = 0;
+            node->child = node->parent = node->lsibling = node->rsibling = nullptr;
+
+            // If the Heap is empty just add it as the first element of the Heap
+            if (this->isEmpty())
+            {
+                this->head = node;
+                this->minRoot = node;
+                this->size++;
+            }
+            // Otherwise add it to the root level list to the left of the current root
+            else
+            {
+                bin_node<Object> * ptr = this->head;
+                this->head = node;
+                node->rsibling = ptr;
+                ptr->lsibling = node;
+                if (node->key < minRoot->key)
+                {
+                    minRoot = node;
+                }
+                this->size++;
+            }
+        }
+
+/*************************************************************************************
+                            Linking/Merging
+*************************************************************************************/
+        
+
+        /* A helper function to merge two trees of the same degree
+            input: two bin_nodes* to merge
+            output: pointer to the parent node 
+            complexity: O(1)*/
+        bin_node<Object>* linkTrees(bin_node<Object> * tree1, bin_node<Object>* tree2)
+        {
+            // Make the root with the greater key the child of the other root
+            if(tree1->key < tree2->key){
+                    /* FOR TEST REMOVE BEFORE SUBMISSION */
+                    cout << "Got tree1 < tree2 case" << endl;
+                    // Remove tree2 from root level list by linking Lsib of tree2 to Rsib of tree2
+                    if(tree2->lsibling != nullptr)
+                    {
+                        tree2->lsibling->rsibling = tree2->rsibling;
+                    }
+                    else
+                    {
+                        tree2->rsibling->lsibling = nullptr;
+                    }
+                    if (tree2->rsibling != nullptr)
+                    {
+                        tree2->rsibling->lsibling = tree2->lsibling;
+                    }
+                    else
+                    {
+                        tree2->lsibling->rsibling = nullptr;
+                    }
+                    // Make tree2 leftmost child of tree1 and link current child node to new child
+                    tree2->parent = tree1;
+                    tree2->rsibling = tree1->child;
+                    if (tree2->rsibling != nullptr)
+                    {
+                        tree2->rsibling->lsibling = tree2;
+                    }
+                    tree2->lsibling = nullptr;
+                    tree1->child = tree2;
+                    tree1->degree += 1;
+                    // Move head pointer if tree2 was head
+                    if (this->head == tree2)
+                    {
+                        this->head = tree1;
+                    }
+
+                    return tree1;
+            }
+            else
+            {
+                    /* FOR TEST REMOVE BEFORE SUBMISSION */
+                    cout << "Got tree2 < tree1 case" << endl;
+                    // Remove tree1 from root level list by linking Lsib of tree1 to Rsib of tree1
+                    if(tree1->lsibling != nullptr)
+                    {
+                        tree1->lsibling->rsibling = tree1->rsibling;
+                    }
+                    else
+                    {
+                        tree1->rsibling->lsibling = nullptr;
+                    }
+                    if (tree1->rsibling != nullptr)
+                    {
+                        tree1->rsibling->lsibling = tree1->lsibling;
+                    }
+                    else
+                    {
+                        tree1->lsibling->rsibling = nullptr;
+                    }
+                    // Make tree1 leftmost child of tree2 and link current child node to new child
+                    tree1->parent = tree2;
+                    tree1->rsibling = tree2->child;
+                    if (tree1->rsibling != nullptr)
+                    {
+                        tree1->rsibling->lsibling = tree1;
+                    }
+                    tree1->lsibling = nullptr;
+                    tree2->child = tree1;
+                    tree2->degree += 1;
+                    // Move head pointer
+                    if (this->head == tree1)
+                    {
+                        this->head = tree2;
+                    }
+
+                    return tree2;
+            }
+        }
+
+        /* Traverses root level of heap and merges trees of the same degree until all the trees are of different degrees
+            input (none): starts at heap's head
+            output(void): operations done on same heap
+            Worst case complexity: O(n) -> Amortized complexity: O(logn)
+        */
+        void merge()
+        {
+            bin_node<Object>* ptr = head;
+            int degree;
+            
+            while (ptr != nullptr)
+            {   
+                // Get current tree's degree
+                degree = ptr->degree;
+                // Check the array for a tree of the same degree if it exists, 
+                if (this->array[degree] != nullptr)
+                {
+                    // Merge the two nodes of same degree and set ptr to new tree, re-enter while loop with new ptr which has degree+1
+                    ptr = linkTrees(this->array[degree], ptr);
+                    // Clear the array slot for old degree
+                    this->array[degree] = nullptr;
+                }
+                // Otherwise if the array slot is null, add the ptr to the array and move the pointer to the right
+                else
+                {
+                    this->array[degree] = ptr;
+                    ptr = ptr->rsibling;
+                }
+            }
+        }
+
+        // This isn't quite right yet, but it is WIP
+        void printHeap(bin_node<Object>* ptr ){
+            
+            while (ptr != nullptr)
+            {
+                cout << "Node id: " <<  ptr->item->id << " Degree: " << ptr->degree;
+                if (ptr->child != nullptr)
+                {
+                    cout << endl << " with children: ";
+                    printHeap(ptr->child);
+                }
+                cout << " <-> ";
+                ptr = ptr->rsibling;
+            }
+            cout << endl;
+        }
+
+        bin_node<Object>* popMin()
+        {
+
+        }
+ 
+    protected:
+   
+
+};
+
+
+
+#endif
---
+++ binomialHeapTest.cpp
@@ -0,0 +1,95 @@
+#include "binomialHeap.h"
+
+typedef struct node{
+   struct arc *first; /* first arc in linked list */
+   int id;  // The number of the vertex in this node
+   int key;  /* Distance estimate, named key to reuse heap code*/
+   int P;  /* Predecessor node in shortest path */
+   int position;  /* Position of node in heap, from 0 to Nm, where 0 is best */
+   } nodeitem;
+
+
+int main(){
+
+    BinomialHeap<nodeitem> * test = new BinomialHeap<nodeitem>;
+    nodeitem* nodeTest = new nodeitem;
+    nodeitem* nodeTest2 = new nodeitem;
+    nodeitem* nodeTest3 = new nodeitem;
+    nodeitem* nodeTest4 = new nodeitem;
+    nodeitem* nodeTest5 = new nodeitem;
+
+
+    bin_node<nodeitem>* bin_test;
+
+    nodeTest->first = nullptr;
+    nodeTest->id = 1;
+    nodeTest->key = 0;
+    nodeTest->P = 1;
+    nodeTest->position = 0;
+
+    nodeTest2->first = nullptr;
+    nodeTest2->id = 2;
+    nodeTest2->key = 1;
+    nodeTest2->P = 0;
+    nodeTest2->position = 0;
+
+    nodeTest3->first = nullptr;
+    nodeTest3->id = 3;
+    nodeTest3->key = -1;
+    nodeTest3->P = 0;
+    nodeTest3->position = 0;
+
+    nodeTest4->first = nullptr;
+    nodeTest4->id = 4;
+    nodeTest4->key = 3;
+    nodeTest4->P = 2;
+    nodeTest4->position = 0;
+    
+    nodeTest5->first = nullptr;
+    nodeTest5->id = 5;
+    nodeTest5->key = 4;
+    nodeTest5->P = 2;
+    nodeTest5->position = 0;
+
+    test->insert(nodeTest3, nodeTest3->key);
+    test->insert(nodeTest4, nodeTest4->key);
+    test->insert(nodeTest, nodeTest->key);
+    test->insert(nodeTest2, nodeTest2->key);
+    test->insert(nodeTest5, nodeTest4->key);
+
+
+    test->printHeap(test->head);
+    
+    test->merge();
+
+    test->printHeap(test->head);
+
+    bin_node<nodeitem>* ptr = test->head;
+
+    cout << "Root Level is: ";
+    while (ptr != nullptr)
+    {
+        cout << ptr->item->id<< "deg:" << ptr->degree << "<->";
+        ptr = ptr->rsibling;
+    }
+    cout << endl;
+
+    cout << "MinRoot is: " << test->minRoot->item->id << " with key: " << test->minRoot->key << endl;
+
+    //bin_test = test->linkTrees(test->head, test->head->rsibling);
+
+    //cout << "Bin_test = " << bin_test->item->id << endl;
+
+    //test->printHeap(test->head);
+
+    //test->linkTrees(test->head->rsibling, test->head->rsibling->rsibling);
+
+    //test->printHeap(test->head);
+
+    //test->linkTrees(test->head, test->head->rsibling);
+
+    //test->printHeap(test->head);
+
+    return 0;
+}
+
